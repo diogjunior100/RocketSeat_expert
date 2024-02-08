6069150042976ca92d41d59ab3ab70c2a0b726a0
diogjunior100 <diogjunior10071@gmail.com>
2024-02-08 10:32:17 -0300

    Add: new files

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_create_controller import CreateTagController
 
 class TagCreatorView:
     ''''
@@ -7,11 +8,14 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest):
+        tag_create_controller = CreateTagController()
+        
         body = http_request.body
         product_code = body["product_code"]
 
         # logica de regra de negocio
+        formatted_response = tag_create_controller.create(product_code)
 
         #retorno http
-        return HttpResponse(status_code = 200, body = {"hello": "world"})
+        return HttpResponse(status_code = 200, body = formatted_response)
     
